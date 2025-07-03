f8f37c84b377c7feba2c0119eb53cf39ec38df56
devPesto <pestonotpasta@gmail.com>
2025-07-04 00:35:23 +0200

    Finish marker + icon tooltip updating

--- src/main/java/com/gmail/goosius/siegewar/integration/map/MapIntegration.java
+++ src/main/java/com/gmail/goosius/siegewar/integration/map/MapIntegration.java
@@ -6,12 +6,18 @@ import com.gmail.goosius.siegewar.enums.SiegeSide;
 import com.gmail.goosius.siegewar.enums.SiegeStatus;
 import com.gmail.goosius.siegewar.objects.BattleSession;
 import com.gmail.goosius.siegewar.objects.Siege;
+import com.gmail.goosius.siegewar.settings.SiegeWarSettings;
+import com.palmergames.bukkit.towny.TownyEconomyHandler;
+import com.palmergames.bukkit.towny.object.Translation;
+import com.palmergames.util.StringMgmt;
+import org.apache.commons.lang.WordUtils;
 
 import java.util.*;
 
 public abstract class MapIntegration {
 	protected static final String DORMANT_BANNER_ICON_ID = "siegewar.dormant";
 	protected static final String ACTIVE_BANNER_ICON_ID = "siegewar.active";
+	protected final Map<UUID, SiegeIcon> map = new HashMap<>();
 	protected final SiegeWar plugin;
 
 	protected MapIntegration(SiegeWar plugin) {
@@ -27,9 +33,40 @@ public abstract class MapIntegration {
 	public abstract void removeMarker(Siege siege);
 
 	public void updateSiegeMarkers() {
+		for (Siege siege : SiegeController.getSieges()) {
 
+			// Delete marker for sieges that aren't active or not present
+			if (siege.getStatus() != SiegeStatus.IN_PROGRESS) {
+				removeMarker(siege);
+				continue;
+			}
+
+			UUID id = siege.getDefender().getUUID();
+			SiegeIcon icon = map.get(id);
+			boolean dormant = icon == SiegeIcon.ACTIVE && isSiegeDormant(siege);
+			map.put(id, dormant ? SiegeIcon.DORMANT : SiegeIcon.ACTIVE);
+			updateMarker(siege, dormant);
+		}
 	}
 
+	protected String iconToolTip(Siege siege) {
+		List<String> lines = new ArrayList<>();
+		String name = Translation.of("dynmap_siege_title", siege.getAttackerNameForDisplay(), siege.getDefenderNameForDisplay());
+		lines.add(Translation.of("dynmap_siege_town", siege.getTown().getName()));
+		lines.add(Translation.of("dynmap_siege_type", siege.getSiegeType().getName()));
+		if (TownyEconomyHandler.isActive())
+			lines.add(Translation.of("dynmap_siege_war_chest", TownyEconomyHandler.getFormattedBalance(siege.getWarChestAmount())));
+
+		lines.add(Translation.of("dynmap_siege_progress", siege.getNumBattleSessionsCompleted(), SiegeWarSettings.getSiegeDurationBattleSessions()));
+		lines.add(Translation.of("dynmap_siege_status", siege.getStatus().getName()));
+		lines.add(Translation.of("dynmap_siege_balance", siege.getSiegeBalance()));
+		lines.add(Translation.of("dynmap_siege_banner_control",
+				WordUtils.capitalizeFully(siege.getBannerControllingSide().name())
+						+ (siege.getBannerControllingSide() == SiegeSide.NOBODY ? "" : " (" + siege.getBannerControllingResidents().size() + ")")));
+		lines.add(Translation.of("dynmap_siege_battle_points", siege.getFormattedAttackerBattlePoints(), siege.getFormattedDefenderBattlePoints()));
+		lines.add(Translation.of("dynmap_siege_battle_time_left", siege.getFormattedBattleTimeRemaining()));
+		return "<b>" + name + "</b><hr>" + StringMgmt.join(lines, "<br>");
+	}
 
 	/**
 	 * A siege is inactive if there is no significant activity there (e.g., kills, banner control).
@@ -44,5 +81,9 @@ public abstract class MapIntegration {
 				&& siege.getDefenderBattlePoints() == 0
 				&& siege.getBannerControllingSide() == SiegeSide.NOBODY
 				&& siege.getBannerControlSessions().isEmpty());
+	}
+
+	protected enum SiegeIcon {
+		ACTIVE, DORMANT
 	}
 }
